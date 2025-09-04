20e1dff7acb3da43d5166f47369c88877b8d4e1a
MaximoLadner <mamoladner@gmail.com>
2025-09-04 09:12:19 -0300

    Avances en Home-Profesor.jsx

--- src/pages/profesor/Home-Profesor.jsx
+++ src/pages/profesor/Home-Profesor.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import SideBarProfesor from '../../components/sidebar/sidebar'
 
 export default function HomeProfesor() {
   return (
-    <div>
+    <>
+     <div>
       <h1>Soy el profesor</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque animi molestiae ea laudantium facere minima voluptas, consectetur vitae sit? Perspiciatis voluptas eligendi ex doloremque voluptatibus explicabo aliquam eos temporibus commodi.</p>
     </div>
+    </>
   )
 }
