97081561c15554c7972744c467ce2a717366c97f
naBogado <bogado.nico17@gmail.com>
2025-09-08 15:54:38 -0300

    MQ Home Alumno, falta funcionalidad

--- src/components/sidebar/sidebar-alumno.jsx
+++ src/components/sidebar/sidebar-alumno.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import mas from "../../images/mas.png";
-import list from "../../images/list.png";
 import unirse from "../../images/unirse.png";
 import calendario from "../../images/calendario.png";
 import perfil from "../../images/perfil.png";
@@ -9,7 +8,7 @@ import dashboard from "../../images/dashboard.png";
 
 export default function SideBarAlumno({ onChangeVista }) {
   return (
-    <aside className="flex flex-col justify-between m-[1rem] gap-1 bg-white shadow-gray-300 rounded-2xl w-fit text-[#0D0E0E]">
+    <aside className="hidden md:flex flex-col justify-between m-[1rem] gap-1 bg-white shadow-gray-300 rounded-2xl w-fit text-[#0D0E0E]">
       <div className="gap-1 mt-4">
         {/* Datos alumno */}
         <div className="w-55 flex flex-row items-center gap-2 ml-4">
@@ -43,14 +42,6 @@ export default function SideBarAlumno({ onChangeVista }) {
           </button>
 
           <button
-            onClick={() => onChangeVista("")}
-            className="w-55 text-left rounded-2xl flex flex-row h-12 items-center gap-1 pr-2.5 pl-2.5 hover:bg-[#137fec] hover:text-white hover:shadow-sm transition-all duration-200"
-          >
-            <img src={list} alt="svg de una lista" className="w-5 h-5 mr-1" />
-            Mis calificaciones
-          </button>
-
-          <button
             onClick={() => onChangeVista("unirse")}
             className="w-55 text-left rounded-2xl flex flex-row h-12 items-center gap-1 pr-2.5 pl-2.5 hover:bg-[#137fec] hover:text-white hover:shadow-sm transition-all duration-200"
           >
@@ -73,7 +64,7 @@ export default function SideBarAlumno({ onChangeVista }) {
       </div>
 
       {/* Sección inferior */}
-      <div className="ml-4 mr-4 mb-3">
+      <div className="ml-4 mr-4 mb-3 flex flex-col gap-2">
         <button
           onClick={() => onChangeVista("perfil")}
           className="w-55 text-left rounded-2xl flex flex-row h-12 items-center gap-1 pr-2.5 pl-2.5 font-normal text-gray-700 hover:bg-[#137fec] hover:text-white hover:shadow-sm transition-all duration-200"
